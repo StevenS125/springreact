1f574d1769b77ba98b6831fb91cdaad44feefc70
Steven Snyder <sts12589@gmail.com>
2020-09-10 17:26:56 -0400

    first commit

---
+++ src/main/java/com/example/springreact/SpringreactApplication.java
@@ -0,0 +1,13 @@
+package com.example.springreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringreactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringreactApplication.class, args);
+	}
+
+}
